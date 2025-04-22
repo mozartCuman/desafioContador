67006490909a8b808e77680abe6e26e1a54de0b0
mozartCuman <mozartcumana@gmail.com>
2025-04-22 19:39:13 -0300

    atualização da classe Sobre.java

--- controleFluxo/Sobre.java
+++ controleFluxo/Sobre.java
@@ -2,11 +2,16 @@ package controleFluxo;
 
 public class Sobre {
     public static void sobre() {
-        System.out.println("\n*********************************************************");
-        System.out.println("Projeto Desenvolvido por: Mozart Cuman");
-        System.out.println("Email: mozartcumana@gmail.com");
-        System.out.println("https://github.com/mozartCuman");
-        System.out.println("*********************************************************");
-       }
-    
+        System.out.println("\n*******************************************************************************");
+        System.out.println("*                                                                             *");
+        System.out.println("*                      Projeto Desenvolvido por:                              *");
+        System.out.println("*                           Mozart Cuman                                      *");
+        System.out.println("*                                                                             *");
+        System.out.println("*                                Contatos:                                    *");
+        System.out.println("*          Email: mozartcumana@gmail.com                                      *");
+        System.out.println("*          GitHub: https://github.com/mozartCuman                             *");
+        System.out.println("*          LinkedIn: https://www.linkedin.com/in/mozart-cuman-albuquerque/    *");
+        System.out.println("*                                                                             *");
+        System.out.println("*******************************************************************************");
+    }
 }
